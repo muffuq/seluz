adbfc0d476c0525b784fa1b9f31debf96688c1e6
Ufuk <82918090+muffuq@users.noreply.github.com>
2022-12-24 15:31:53 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,26 @@
+//Cursor
+
+var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+var a = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + 'px';
+  cursorinner.style.top = y + 'px';
+});
+
+//Swiper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+});
+
